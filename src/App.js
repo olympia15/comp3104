97023383758c6875625d1395f6fa41f818404c42
Olympia Santaguida <161262938+olympia15@users.noreply.github.com>
2025-10-15 13:32:34 -0400

    Dep inclusions (#3)
    * GitHub action for github pages added
    
    * name edited

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101469745 || Olympia Santaguida-Antunes || DevOps</h5>
+      <h5>Developed By: 101469745 || Olympia Santaguida-Antunes || COMP3104 - DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
